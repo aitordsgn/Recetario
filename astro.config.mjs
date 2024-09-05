9e8a10ec1a52d34ca0ad8dbe0d363df4a41e47f5
aitordsgn <aitordsgn@gmail.com>
2024-09-05 16:11:45 +0200

    chore: Update Astro template to minimal version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
-import md from '@astrojs/markdown-remark';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [md()],
-});
+export default defineConfig({});
